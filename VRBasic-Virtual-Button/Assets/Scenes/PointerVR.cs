d16f462d0157ef64a48288e44018ea8897418944
Railway-RMUTP <railwayrmutp@gmail.com>
2020-05-27 17:23:26 +0700

    Init

---
+++ VRBasic-Virtual-Button/Assets/Scenes/PointerVR.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerVR : MonoBehaviour
+{
+    public Material active, Default;
+    private Renderer myRenderer;
+
+    private void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        SetGazedAt(false);
+    }
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        if (Default != null && active != null)
+        {
+            myRenderer.material = gazedAt ? active : Default;
+            return;
+        }
+    }
+}
